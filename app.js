bb490f6adc5deaff0703f60a6daf230b169bb841
mr-Bones3108 <nikhil788p@gmail.com>
2023-10-06 08:27:08 +0530

    Add and test basic math operations: add, subtract, multiply, divide

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("hi i am liked")
+// alert("hi i am liked")
+
+
+function add(a,b){
+    return a+b
+}
+
+function sub(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
