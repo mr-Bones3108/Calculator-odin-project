6822cd00001dfd5573305d61fc69f8ccf008f5f5
mr-Bones3108 <nikhil788p@gmail.com>
2023-10-06 10:33:31 +0530

    add javaScript functions on each button to perfrom calculations

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
-// alert("hi i am liked")
+const buttons = document.querySelectorAll(".myBtn")
+const display = document.getElementById("display")
 
 
-function add(a,b){
-    return a+b
+buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        const btnValue = button.value;
+        if(btnValue === '='){
+            const calc = new  Calculator;
+            display.value = calc.calculate(display.value)
+            return 
+        }
+        if(btnValue === 'del'){
+            if (btnValue === 'del') {
+                deleteFromStr();
+                return; // Make sure to return after calling the function
+            }
+        }
+        if(btnValue === 'ac'){
+            display.value = ''; 
+            return
+        }
+        display.value +=btnValue
+    })
+
+})
+
+function deleteFromStr() {
+    if (display.value.length > 0) {
+        display.value = display.value.slice(0, -1); // Remove the last character
+        console.log(display.value);
+    }
 }
 
-function sub(a,b){
-    return a-b
+function Calculator(){
+
+    this.calculate=function(str){
+        let split = str.split(/(\d+|\+)/).filter(Boolean);
+        a=+split[0]
+        op=split[1]
+        b=+split[2]
+        // console.log(split)
+
+
+        if(!this.method[op] || isNaN(a) || isNaN(b)){
+            return NaN
+        }
+        return this.method[op](a,b);
+    }
+
+    this.method={
+        "+":(a,b)=>a+b,
+        "-":(a,b)=>a-b,
+        "*":(a,b)=>a*b,
+        "/":(a,b)=>a/b
+    }
+
+    this.addMethod = function(name,func){
+        this.method[name]=func
+    }
 }
 
-function multiply(a,b){
-    return a*b
-}
 
-function divide(a,b){
-    return a/b
-}
 
